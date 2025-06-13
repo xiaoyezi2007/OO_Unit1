5328501d06ef7e70b66424288279179883ed636e
xiaoyezi2007 <2897107430@qq.com>
2025-06-13 18:14:11 +0800

    autojudge

--- src/MainClass.java
+++
@@ -1,8 +0,0 @@
-public class MainClass {
-    public static void main(String[] args) {
-        Input input = new Input();
-        Expr expr = input.target();
-        expr = expr.simplify();
-        expr.print();
-    }
-}
